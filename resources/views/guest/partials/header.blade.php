e73a3d8813e004610e3763b9c7da814f447897e3
AndreaNocciolini <andreanocciolini93@gmail.com>
2022-02-14 11:57:59 +0100

    add ternary for active class (header)

--- resources/views/guest/partials/header.blade.php
+++ resources/views/guest/partials/header.blade.php
@@ -4,16 +4,16 @@
     </div>
 
     <ul class="header-ul">
-        <li class="header-li"><a class="header-a" href="">CHARACTERS</a></li>
-        <li class="header-li"><a class="active header-a" href=" {{ route('home') }}">COMICS</a></li>
-        <li class="header-li"><a class="header-a" href="{{ route('movies') }}">MOVIES</a></li>
-        <li class="header-li"><a class="header-a" href="">TV</a></li>
-        <li class="header-li"><a class="header-a" href="">GAMES</a></li>
-        <li class="header-li"><a class="header-a" href="">COLLECTIBLES</a></li>
-        <li class="header-li"><a class="header-a" href="">VIDEOS</a></li>
-        <li class="header-li"><a class="header-a" href="">FANS</a></li>
-        <li class="header-li"><a class="header-a" href="">NEWS</a></li>
-        <li class="header-li"><a class="header-a" href="">SHOP</a></li>
+        <li class="header-li"><a class="header-a {{ 'characters' === Route::currentRouteName() ? 'active' : '' }}" href="">CHARACTERS</a></li>
+        <li class="header-li"><a class="header-a {{ 'home' === Route::currentRouteName() ? 'active' : '' }}" href=" {{ route('home') }}">COMICS</a></li>
+        <li class="header-li"><a class="header-a {{ 'movies' === Route::currentRouteName() ? 'active' : '' }}" href="{{ route('movies') }}">MOVIES</a></li>
+        <li class="header-li"><a class="header-a {{ 'tv' === Route::currentRouteName() ? 'active' : '' }}" href="">TV</a></li>
+        <li class="header-li"><a class="header-a {{ 'games' === Route::currentRouteName() ? 'active' : '' }}" href="">GAMES</a></li>
+        <li class="header-li"><a class="header-a {{ 'collectibles' === Route::currentRouteName() ? 'active' : '' }}" href="">COLLECTIBLES</a></li>
+        <li class="header-li"><a class="header-a {{ 'videos' === Route::currentRouteName() ? 'active' : '' }}" href="">VIDEOS</a></li>
+        <li class="header-li"><a class="header-a {{ 'fans' === Route::currentRouteName() ? 'active' : '' }}" href="">FANS</a></li>
+        <li class="header-li"><a class="header-a {{ 'news' === Route::currentRouteName() ? 'active' : '' }}" href="">NEWS</a></li>
+        <li class="header-li"><a class="header-a {{ 'shop' === Route::currentRouteName() ? 'active' : '' }}" href="">SHOP</a></li>
     </ul>
 </div>
 
